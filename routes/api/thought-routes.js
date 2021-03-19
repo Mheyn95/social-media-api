ac60d9970708260b722213dc814f2b085cc0b92d
mheyn95 <mheyn95@gmail.com>
2021-03-19 13:24:32 -0600

    added routes

---
+++ routes/api/thought-routes.js
@@ -0,0 +1,27 @@
+const router = require("express").Router();
+const {
+  getAllThoughts,
+  getThoughtById,
+  addThought,
+  updateThought,
+  removeThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughts-controller");
+
+// /api/thoughts
+router.route("/").get(getAllThoughts).post(addThought);
+// /api/thoughts/<thoughtId>
+router
+  .route("/:thoughtId")
+  .get(getThoughtById)
+  .put(updateThought)
+  .delete(removeThought);
+
+// /api/thoughts/<thoughtId>/reactions
+router.route("/:thoughtId/reactions").post(addReaction);
+
+// /api/thoughts/<thoughtId>/reactions/<reactionID>
+router.route("/:thoughtId/reactions/:reactionId").delete(removeReaction);
+
+module.exports = router;
